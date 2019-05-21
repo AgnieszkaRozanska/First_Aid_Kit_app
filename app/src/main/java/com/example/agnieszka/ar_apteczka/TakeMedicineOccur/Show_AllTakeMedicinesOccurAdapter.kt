a1f6800f3b3f84065daf762ef8a70e845efa9217
AgnieszkaRozanska <agniroz767@student.polsl.pl>
2019-05-21 11:51:53 +0200

    Change the look of Card View

--- app/src/main/java/com/example/agnieszka/ar_apteczka/TakeMedicineOccur/Show_AllTakeMedicinesOccurAdapter.kt
+++ app/src/main/java/com/example/agnieszka/ar_apteczka/TakeMedicineOccur/Show_AllTakeMedicinesOccurAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.agnieszka.ar_apteczka.TakeMedicineOccur
 
 import android.content.Context
+import android.graphics.Color
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.example.agnieszka.ar_apteczka.R
+import kotlinx.android.synthetic.main.card_view_all_tak_medicines_occur.view.*
 
 
 class card_view_All_TakeMedicinesOccur(context: Context, var takeMedicineOccurList: ArrayList<TakeMedicineOccur>): RecyclerView.Adapter<MyViewHolderTakeMedOccur>() {
@@ -23,9 +25,23 @@ class card_view_All_TakeMedicinesOccur(context: Context, var takeMedicineOccurLi
 
     override fun onBindViewHolder(holder: MyViewHolderTakeMedOccur, position: Int) {
         holder.medName.setText(takeMedicineOccurList.elementAt(position).medicineType)
-        holder.medDose.setText(takeMedicineOccurList.elementAt(position).dose.toString())
-        holder.medTimeOfDay.setText(takeMedicineOccurList.elementAt(position).timeOfDay)
+        holder.medDose.setText("Dawka:       "+takeMedicineOccurList.elementAt(position).dose.toString())
+        holder.medTimeOfDay.setText("Pora dnia zażycia:    "+takeMedicineOccurList.elementAt(position).timeOfDay)
         holder.medAfterBeforeMeal.setText(takeMedicineOccurList.elementAt(position).beforeAfterMeal)
+
+
+
+        val cardView_medicine = holder.view.Take_Medicine_Occur_cardView
+
+        if (takeMedicineOccurList.elementAt(position).timeOfDay=="Rano") {
+            cardView_medicine.setCardBackgroundColor(Color.parseColor("#F3F1B0"))
+        }
+        if (takeMedicineOccurList.elementAt(position).timeOfDay=="Wieczór") {
+            cardView_medicine.setCardBackgroundColor(Color.parseColor("#FFC9CCF1"))
+        }
+        if (takeMedicineOccurList.elementAt(position).timeOfDay=="Popołudnie") {
+            cardView_medicine.setCardBackgroundColor(Color.parseColor("#F366B1"))
+        }
     }
 }
 
