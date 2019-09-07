2340a285e254552643bf9f8eff10dfaca462d0df
AgnieszkaRozanska <agniroz767@student.polsl.pl>
2019-09-07 22:10:01 +0200

    Add information about lack of medicines - TakeMedOccur

--- app/src/main/java/com/example/agnieszka/ar_apteczka/takeMedicineOccur/showAllTakeMedicineOccur/ActivityShowAllTakeMedicineOccur.kt
+++ app/src/main/java/com/example/agnieszka/ar_apteczka/takeMedicineOccur/showAllTakeMedicineOccur/ActivityShowAllTakeMedicineOccur.kt
@@ -2,8 +2,10 @@ package com.example.agnieszka.ar_apteczka.takeMedicineOccur.showAllTakeMedicineO
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.TextView
 import com.example.agnieszka.ar_apteczka.Menu
 import com.example.agnieszka.ar_apteczka.R
 import com.example.agnieszka.ar_apteczka.sqlconnctor.SQLConector
@@ -13,6 +15,7 @@ import com.facebook.stetho.Stetho
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import kotlinx.android.synthetic.main.activity_all_take_medicine_occur_recycler_view.*
 import okhttp3.OkHttpClient
+import java.lang.Exception
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -22,6 +25,20 @@ class ActivityShowAllTakeMedicineOccur : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_take_medicine_occur_recycler_view)
+
+        textViewInformLackOfMorningMed.visibility = TextView.INVISIBLE
+        textViewInformLackOfAfternoonMedicines.visibility = TextView.INVISIBLE
+        textViewInformLackOfMorningMed.visibility = TextView.INVISIBLE
+        val handler = Handler()
+        val runnable = object : Runnable {
+            override fun run() {
+                try {
+                    setTextIfListIsEmpty()
+                } catch (e: Exception) {}
+                handler.postDelayed(this, 300)
+            }
+        }
+        handler.postDelayed(runnable, 300)
     }
 
 
@@ -57,7 +74,20 @@ class ActivityShowAllTakeMedicineOccur : AppCompatActivity() {
             )
     }
 
+    private fun setTextIfListIsEmpty(){
+        val sqlConector = SQLConector(this)
+        val  takeMedicinesOccurListMORNING=sqlConector.getAllTakeMedicineOccur(getString(R.string.Morning))
+        val  takeMedicinesOccurListMIDDAY=sqlConector.getAllTakeMedicineOccur(getString(R.string.Midday))
+        val  takeMedicinesOccurListEVENING=sqlConector.getAllTakeMedicineOccur(getString(R.string.Evening))
+        if(takeMedicinesOccurListMORNING.isNullOrEmpty()) textViewInformLackOfMorningMed.visibility = TextView.VISIBLE
+        else textViewInformLackOfMorningMed.visibility = TextView.INVISIBLE
 
+        if(takeMedicinesOccurListMIDDAY.isNullOrEmpty()) textViewInformLackOfAfternoonMedicines.visibility = TextView.VISIBLE
+        else textViewInformLackOfAfternoonMedicines.visibility = TextView.INVISIBLE
+
+        if(takeMedicinesOccurListEVENING.isNullOrEmpty()) textViewInformLackOfEveningMedicines.visibility = TextView.VISIBLE
+        else textViewInformLackOfEveningMedicines.visibility = TextView.INVISIBLE
+    }
 }
 
 
