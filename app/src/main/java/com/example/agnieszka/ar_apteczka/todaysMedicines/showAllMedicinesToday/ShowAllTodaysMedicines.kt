5714cadc44344cbc4b8836f752e78fee1c0b5160
AgnieszkaRozanska <agniroz767@student.polsl.pl>
2019-09-07 20:34:50 +0200

    Add information about taking all medications for a given day

--- app/src/main/java/com/example/agnieszka/ar_apteczka/todaysMedicines/showAllMedicinesToday/ShowAllTodaysMedicines.kt
+++ app/src/main/java/com/example/agnieszka/ar_apteczka/todaysMedicines/showAllMedicinesToday/ShowAllTodaysMedicines.kt
@@ -3,6 +3,7 @@ package com.example.agnieszka.ar_apteczka.todaysMedicines.showAllMedicinesToday
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.widget.LinearLayoutManager
 import android.widget.TextView
 import com.example.agnieszka.ar_apteczka.Menu
@@ -14,10 +15,13 @@ import com.facebook.stetho.Stetho
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import kotlinx.android.synthetic.main.activity_show_all_todays_medicines.*
 import okhttp3.OkHttpClient
+import java.lang.Exception
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class ShowAllTodaysMedicines : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,17 +35,29 @@ class ShowAllTodaysMedicines : AppCompatActivity() {
             startActivity(goToMenu)
         }
 
+
+      val handler = Handler()
+        val runnable = object : Runnable {
+            override fun run() {
+                try {
+                    setTextIfListIsEmpty()
+                } catch (e: Exception) {}
+                handler.postDelayed(this, 300)
+            }
+        }
+        handler.postDelayed(runnable, 300)
+
+
+
     }
      override fun onResume() {
         super.onResume()
         val sqlConector = SQLConector(this)
         var dataToday = takeDataToday()
-        val  takeTodayMedicinesListMORNING=sqlConector.getAllTakeMedicinesToday(getString(R.string.Morning), dataToday)
+        var takeTodayMedicinesListMORNING=sqlConector.getAllTakeMedicinesToday(getString(R.string.Morning), dataToday)
+        var takeTodayMedicinesListMIDDAY=sqlConector.getAllTakeMedicinesToday(getString(R.string.Midday), dataToday)
+        var takeTodayMedicinesListEVENING=sqlConector.getAllTakeMedicinesToday(getString(R.string.Evening), dataToday)
 
-        val  takeTodayMedicinesListMIDDAY=sqlConector.getAllTakeMedicinesToday(getString(R.string.Midday), dataToday)
-        val  takeTodayMedicinesListEVENING=sqlConector.getAllTakeMedicinesToday(getString(R.string.Evening), dataToday)
-        if(takeTodayMedicinesListEVENING.isNullOrEmpty()) textViewEveningMedicines.visibility = TextView.VISIBLE
-        else textViewEveningMedicines.visibility = TextView.INVISIBLE
         recyler_view_All_Medicines_To_Take_Today_Morning.layoutManager=
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL ,false)
         recyler_view_All_Medicines_To_Take_Today_Morning.adapter=
@@ -85,8 +101,20 @@ class ShowAllTodaysMedicines : AppCompatActivity() {
         return dateTodayString.toString()
     }
 
-    public fun setTextIfListIsEmpty(takeTodayMedicinesListEVENING : ArrayList<MedicineToTake>){
+    private fun setTextIfListIsEmpty(){
+        val sqlConector = SQLConector(this)
+        var dataToday = takeDataToday()
+        var takeTodayMedicinesListMORNING=sqlConector.getAllTakeMedicinesToday(getString(R.string.Morning), dataToday)
+        var takeTodayMedicinesListMIDDAY=sqlConector.getAllTakeMedicinesToday(getString(R.string.Midday), dataToday)
+        var takeTodayMedicinesListEVENING=sqlConector.getAllTakeMedicinesToday(getString(R.string.Evening), dataToday)
         if(takeTodayMedicinesListEVENING.isNullOrEmpty()) textViewEveningMedicines.visibility = TextView.VISIBLE
+        else textViewEveningMedicines.visibility = TextView.INVISIBLE
+
+        if(takeTodayMedicinesListMIDDAY.isNullOrEmpty()) textViewAfternoonMedicines.visibility = TextView.VISIBLE
+        else textViewAfternoonMedicines.visibility = TextView.INVISIBLE
+
+        if(takeTodayMedicinesListMORNING.isNullOrEmpty()) textViewMorningMedicines.visibility = TextView.VISIBLE
+        else textViewMorningMedicines.visibility = TextView.INVISIBLE
     }
 
 
